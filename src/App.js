2bd7f87bfa6a5a8c7b52647e2b66371e2d2db82b
Jackie Allex <jackieallex@gmail.com>
2026-02-01 22:56:20 -0800

    better border

--- src/App.js
+++ src/App.js
@@ -32,29 +32,29 @@ export default function AnimationStudio() {
       {/* Responsive Border Frame */}
       {/* Top border - full width including corners */}
       <div
-        className="fixed top-0 left-0 right-0 h-[60px] pointer-events-none z-30 hidden md:block"
+        className="fixed top-0 left-0 right-0 h-[69px] lg:h-[84px] xl:h-[99px] 2xl:h-[114px] pointer-events-none z-30 hidden md:block"
         style={{
           backgroundImage: `url(${border})`,
-          backgroundSize: 'max(100vw, 100vh * 1.05) auto',
+          backgroundSize: 'max(100vw, 100vh * 0.9) auto',
           backgroundPosition: 'top center'
         }}
       />
       {/* Left border - from top to bottom */}
       <div
-        className="fixed top-0 bottom-0 left-0 w-[40px] pointer-events-none z-30 hidden md:block"
+        className="fixed top-0 bottom-0 left-0 w-[54px] lg:w-[69px] xl:w-[84px] 2xl:w-[99px] pointer-events-none z-30 hidden md:block"
         style={{
           backgroundImage: `url(${border})`,
-          backgroundSize: 'auto 100%',
+          backgroundSize: 'max(100vw, 100vh * 0.9) auto',
           backgroundPosition: 'left top',
           backgroundRepeat: 'no-repeat'
         }}
       />
       {/* Right border - from top to bottom */}
       <div
-        className="fixed top-0 bottom-0 right-0 w-[40px] pointer-events-none z-30 hidden md:block"
+        className="fixed top-0 bottom-0 right-0 w-[54px] lg:w-[69px] xl:w-[84px] 2xl:w-[99px] pointer-events-none z-30 hidden md:block"
         style={{
           backgroundImage: `url(${border})`,
-          backgroundSize: 'auto 100%',
+          backgroundSize: 'max(100vw, 100vh * 0.9) auto',
           backgroundPosition: 'right top',
           backgroundRepeat: 'no-repeat'
         }}
